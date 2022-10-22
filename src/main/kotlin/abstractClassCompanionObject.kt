4546c66cd48b30d24807d94892a49c58f0c53ecf
inferno086350 <as086350@gmail.com>
2022-10-22 15:06:26 +0530

    New commit

--- src/main/kotlin/abstractClassCompanionObject.kt
+++ src/main/kotlin/abstractClassCompanionObject.kt
@@ -12,14 +12,17 @@ fun main()
 //    Maruti.Wheel.roll() -  Not allowed as object Wheel belongs only to Car class
     val car1 : Car = Maruti(40)
     car1.run()
+    car1.roll()
 }
 
 abstract class Car
 {
     abstract val capacity : Int
+    abstract fun roll()
 
-    companion object Wheel{
-        fun roll() = println("Wheel is Rolling!")
+    companion object Wheel : Car(){
+        override val capacity = 0
+        override fun roll() = println("Wheel is Rolling!")
     }
 
     fun run() = println("Car is running!")
@@ -28,4 +31,5 @@ abstract class Car
 class Maruti(_capacity : Int) : Car()
 {
     override val capacity: Int = _capacity
+    override fun roll() = println("Maruti is rolling!")
 }
